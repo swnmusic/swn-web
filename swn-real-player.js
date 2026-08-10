3e8039acf074d1503ecc6e58a12f095769deff8d
swnmusic <urigoitia@icloud.com>
2026-08-10 23:31:34 +0200

    Adjust Is It You canvas crop

--- swn-real-player.js
+++ swn-real-player.js
@@ -200,7 +200,11 @@
 
       coverVideo.style.objectFit = "cover";
         coverVideo.style.objectPosition =
-          track.title === "Slow Down" ? "center 62%" : "center 42%";
+          track.title === "Slow Down"
+            ? "center 62%"
+            : track.title === "Is It You"
+              ? "center 34%"
+              : "center 42%";
       const newSrc = new URL(track.canvas, document.baseURI).href;
 
       if (coverVideo.src !== newSrc) {
